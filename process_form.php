3d093b34323669efb56e3e0cc2fc94685612d79c
duguayworld <153779837+duguayworld@users.noreply.github.com>
2023-12-14 22:19:05 -0500

    Add files via upload

---
+++ process_form.php
@@ -0,0 +1,29 @@
+<?php
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Get form data
+    $name = $_POST["Name"];
+    $email = $_POST["Email"];
+    $subject = $_POST["Subject"];
+    $message = $_POST["Message"];
+
+    // Create a string with the form data
+    $formData = "Name: $name\nEmail: $email\nSubject: $subject\nMessage: $message\n\n";
+
+    // Define the path to the text file
+    $filePath = "form_data.txt";
+
+    // Append the form data to the text file
+    file_put_contents($filePath, $formData, FILE_APPEND | LOCK_EX);
+
+    // Redirect to a thank you page or back to the form page
+    header("Location: thank_you.html");
+    exit();
+} else {
+    // Handle invalid requests
+    header("HTTP/1.1 400 Bad Request");
+    echo "Invalid request method.";
+    exit();
+}
+?>
+
